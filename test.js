c65d56cb9ee1ec1e7cc2d45d2d1958af7f1f85ac
Tan Theng Lee <thengleetan@gmail.com>
2015-04-21 14:07:06 +0800

    Add initial tests

---
+++ test.js
@@ -0,0 +1,18 @@
+var request = require('supertest');
+var app = require('./app');
+
+
+describe ('Request to the root path', function(){
+
+	it('Returns a 200 status code', function(done){
+		request(app)
+			.get('/')
+			.expect(200)
+			.end(function(err){
+				if (err) throw err;
+				done();
+			});
+	});
+	
+
+});
