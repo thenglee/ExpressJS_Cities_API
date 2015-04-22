88b9a0d3872d71bc7e22e82456e04b414a8906a5
Tan Theng Lee <thengleetan@gmail.com>
2015-04-22 11:41:04 +0800

    No select for production

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ if (process.env.REDISTOGO_URL){
 	client.auth(rtg.auth.split(":")[1]);
 }else{
 	var client = redis.createClient();
+	client.select((process.env.NODE_ENV || 'development').length);
 }
 
 
-client.select(process.env.NODE_ENV.length || 'development'.length);
+
+
 // End Redis connection
 
 // client.hset('cities', 'Lotopia', 'description');
