17cfdbdd7076643a8a8c90a856a5841f54b6ee86
Tan Theng Lee <thengleetan@gmail.com>
2015-04-22 11:21:17 +0800

    Replace in memory with redis

--- app.js
+++ app.js
@@ -6,20 +6,42 @@ var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
-var cities = {
-	'Lotopia': 'some description', 
-	'Caspiana': 'description', 
-	'Indigo': 'description'
-};
+// Redis connection
+var redis = require('redis');
+
+if (process.env.REDISTOGO_URL){
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var client = redis.createClient(rtg.port, rtg.hostname);
+	client.auth(rtg.auth.split(":")[1]);
+}else{
+	var client = redis.createClient();
+}
+
+
+client.select(process.env.NODE_ENV.length || 'development'.length);
+// End Redis connection
+
+// client.hset('cities', 'Lotopia', 'description');
+// client.hset('cities', 'Caspiana', 'description');
+// client.hset('cities', 'Indigo', 'description');
+
 
 app.get('/cities', function(req, res){
-	res.json(Object.keys(cities));
+	client.hkeys('cities', function(err, names){
+		if (err) throw err;
+		res.json(names);
+	});
+	
 });
 
 app.post('/cities', urlencode, function(req, res){
 	var newCity = req.body;
-	cities[newCity.name] = newCity.description;
-	res.status(201).json(newCity.name);
+	client.hset('cities', newCity.name, newCity.description, function(err){
+		if (err) throw err;
+		res.status(201).json(newCity.name);
+	});
+
+	
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 
 describe ('Request to the root path', function(){
 
@@ -43,12 +48,17 @@ describe('Listing cities on /cities', function(){
 	it('Returns initial cities', function(done){
 		request(app)
 			.get('/cities')
-			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+			.expect(JSON.stringify([]), done);
 	});
 });
 
 
 describe('Creating new cities', function(){
+
+	before(function(){
+
+	});
+
 	it('Returns 201 status code', function(done){
 		request(app)
 			.post('/cities')
